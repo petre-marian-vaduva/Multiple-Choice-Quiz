1a45fa28db6bd91c439450db61376bdff0ca7920
Petre-Marian Vaduva <rik_296@yahoo.com>
2021-04-16 00:13:47 +0100

    loop over the questions

--- quiz.js
+++ quiz.js
@@ -9,3 +9,15 @@ const questions = [
     (b) Red\n(c) Blue',
     answer: 'a'}
 ]
+
+const scores = 0;
+
+for(let i =0; i< questions.length; i++){
+    const response = window.prompt(questions[i].prompt);
+    if(response == questions[i].answer){
+        scores++;
+        alert('Correct!')
+    }else{
+        alert('Wrong!')
+    }
+}
