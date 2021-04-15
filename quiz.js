b9b532e1b6a550a86bdae8ee28937b129c2226fc
Petre-Marian Vaduva <rik_296@yahoo.com>
2021-04-15 23:59:11 +0100

    add multiple choice quiz

---
+++ quiz.js
@@ -0,0 +1,11 @@
+const questions = [
+    {prompt: 'What colour are apples?\n(a) Red/Green\n\
+    (b) Purple\n(c) Orange',
+    answer: 'a'},
+    {prompt: 'What colour are bananas?\n(a) Teal\n\
+    (b) Purple\n(c) Yellow',
+    answer: 'c'},
+    {prompt: 'What colour are strawberries?\n(a) Yellow\n\
+    (b) Red\n(c) Blue',
+    answer: 'a'}
+]
